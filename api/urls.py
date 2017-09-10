7ac84a7e28dc7163f0de44afd69adfbb69b12d89
Vimal Sheoran <vimalpsheoran@gmail.com>
2017-09-10 23:57:39 +0530

    Adding a URL store

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+url = {
+    'digitalWrite' : 'http://cloud.boltiot.com/remote/{}/digitalWrite?pin={}&state={}&deviceName={}',
+    'digitalRead' : 'http://cloud.boltiot.com/remote/{}/digitalRead?pin={}&deviceName={}',
+    'analogWrite' : 'http://cloud.boltiot.com/remote/{}/analogWrite?pin=1&value={}&state={}&deviceName={}',
+    'analogRead' : 'http://cloud.boltiot.com/remote/{}/analogRead?pin={}&deviceName={}',
+    'serialBegin' : 'http://cloud.boltiot.com/remote/{}/serialBegin?baud={}&deviceName={}',
+    'serialWrite' : 'http://cloud.boltiot.com/remote/{}/serialWrite?data={}&deviceName={}',
+    'serialRead' : 'http://cloud.boltiot.com/remote/{}/serialRead?till={}&deviceName={}',
+    'version' : 'http://cloud.boltiot.com/remote/{}/version?&deviceName={}',
+    'restart' : 'http://cloud.boltiot.com/remote/{}/restart?&deviceName={}',
+    'isAlive' : 'http://cloud.boltiot.com/remote/{}/isAlive?&deviceName={}',
+}
