0dfb95ae050bbb70b038eefa0831beb0c3b55faf
vimalsheoran <vimalpsheoran@gmail.com>
2018-06-01 13:34:24 +0530

    Unit testing done and module created

--- test.py
+++ test.py
@@ -1,81 +1,94 @@
 from boltiot.bolt import Bolt
-from unittest import TestCase
+import unittest
 import json
+import time
 
-client = Bolt('123','123') # Pass in the API Key and the client ID.
+client = Bolt('7bc48b25-f5c8-4ef3-9477-c599835583da','BOLT3729610') # Pass in the API Key and the client ID.
 
-class BoltTests(TestCase):
+class BoltTests(unittest.TestCase):
     
 # Testing the digital write function.
     
-    def test_digitalWrite():
-        assert_value = json.loads(client.digitalWrite('0','HIGH'))
-        self.assertEqual(assert_value["success"],1)
-        self.assertEqual(assert_value["value"],1)
-
-# Testing the digital read function.
-        
-    def test_digitalRead():
-        assert_value = json.loads(client.digitalRead('0'))
-        self.assertEqual(assert_value["success"],1)
-        self.assertEqual(assert_value["value"],1)
-
-# Testing the analog read function.
-        
-    def test_analogRead():
-        assert_value = json.loads(client.analogRead('A0'))
-        self.assertEqual(assert_value["success"],1)
-        self.assertTrue(0 <= int(assert_value["value"]) <= 254)
+    def test_1(self):
+        assert_value = json.loads(client.digitalWrite('4','HIGH'))
+        self.assertEqual(assert_value["success"],"1")
+        self.assertEqual(assert_value["value"],"1")
+        print("Digital Write Successfull!")
 
 # Testing the analog write function
         
-    def test_analogWrite():
-        assert_value = json.loads(client.analogWrite('A0','100'))
+    def test_3(self):
+        assert_value = json.loads(client.analogWrite('0','100'))
         self.assertEqual(assert_value["success"],"1")
         self.assertEqual(assert_value["value"],"1")
-
+        print("Analog Write Successfull!")
+        
+# Testing the digital read function.
+        
+    def test_2(self):
+        assert_value = json.loads(client.digitalRead('1'))
+        self.assertEqual(assert_value["success"],"1")
+        self.assertEqual(int(assert_value["value"]),1)
+        print("Digital Read Successfull!")
+        
+# Testing the analog read function.
+        
+    def test_4(self):
+        assert_value = json.loads(client.analogRead('A0'))
+        self.assertEqual(assert_value["success"],"1")
+        self.assertTrue(0 <= int(assert_value["value"]) <= 1024)
+        print("Analog Read Succesfull!")
+        
 # Testing the serialBegin() function.
         
-    def test_serialBegin():
-        assert_value = json.loads(client.serialBegin('9600'))
+    def test_5(self):
+        assert_value = json.loads(client.serialBegin("9600"))
         self.assertEqual(assert_value["success"],"1")
         self.assertEqual(assert_value["value"],"Success")
-
+        print("Serial Begin Successfull!")
+        
 # Testing the serialWrite() function.
         
-    def test_serialWrite():
-        assert_value = json.loads(client.serialWrite('5'))
+    def test_6(self):
+        assert_value = json.loads(client.serialWrite('inventrom'))
         self.assertEqual(assert_value["success"],"1")
         self.assertEqual(assert_value["value"],"Serial write Successful")
-
+        print("Serial Write Successfull!")
+        
 # Testing the serialRead() function.
         
-    def test_serialRead():
+    def test_7(self):
         assert_value = json.loads(client.serialRead('10'))
         self.assertEqual(assert_value["success"],"1")
         self.assertEqual(assert_value["value"],"inventrom")
-
-# Testing the restart() function.
+        print("Serial Read Successfull!")
         
-    def test_restart():
-        assert_value = json.loads(client.restart())
-        self.assertEqual(assert_value["success"],"1")
-        self.assertEqual(assert_value["value"],"Restarted")
+# Testing the restart() function.
 
+    def test_restart(self):
+        assert_value = json.loads(client.restart())
+        time.sleep(5)
+        try :
+            self.assertEqual(assert_value["value"],"Restarted")
+        except AssertionError:
+            self.assertEqual(assert_value["value"], "Command timed out")
+        print("Restart Successfull!")
+        
 # Testing the isAlive() function.
         
-    def test_isAlive():
+    def test_8(self):
         assert_value = json.loads(client.isAlive())
         self.assertEqual(assert_value["success"],"1")
         self.assertEqual(assert_value["value"],"alive")
-
+        print("isAlive Successfull!")
+        
 # Testing the isOnline() function
         
-    def test_isOnline():
+    def test_9(self):
         assert_value = json.loads(client.isOnline())
-        self.assertEqual(assert_value["success"],"1")
+        self.assertEqual(str(assert_value["success"]),"1")
         self.assertEqual(assert_value["value"],"online")
-        
+        print("isOnline Successfull!")
 
 if __name__ == "__main__":
     device_status = json.loads(client.isOnline())
@@ -83,5 +96,3 @@ if __name__ == "__main__":
         unittest.main()
     else:
         print("The device is offline test cannot be conducted at the moment. Connect your Bolt to the cloud before testing it.")
-
-
