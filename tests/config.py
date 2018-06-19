edeeff01047306a4482d979d04c9ce77535e6450
vimalsheoran <vimalpsheoran@gmail.com>
2018-06-19 12:17:21 +0530

    Adding configs for testing UART functions

--- tests/config.py
+++ tests/config.py
@@ -12,6 +12,23 @@ GPIO_CONFIG = {
     "ANALOG_WRITE_PIN": '0'
 }
 
+UART_CONFIG = {
+    "SUCCESS_RESPONSE": '1',
+    "FAILED_RESPONSE": '0',
+    "VALID_BAUD_RATE": "9600",
+    "INVALID_BAUD_RATE": "10",
+    "VALID_BAUD_RESPONSE": "Success",
+    "INVALID_BAUD_RESPONSE": "Invalid baud value",
+    "VALID_TILL": "10",
+    "INVALID_TILL": "1000",
+    "VALID_TILL_VALUE": "",
+    "INVALID_TILL_VALUE": "Invalid till value",
+    "VALID_WRITE_VALUE": "hello",
+    "INVALID_WRITE_VALUE": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "VALID_DATA_RESPONSE": "Serial write Successfull",
+    "INVALID_DATA_RESPONSE": "Command timed out"
+}
+
 CREDENTIALS = {
     "API_KEY": "xxxx",
     "DEVICE_ID": "xxxx"
