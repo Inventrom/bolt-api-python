cb5b8fb36341a277688b6ee8d582c4ee92a894ad
vimalsheoran <vimalpsheoran@gmail.com>
2018-06-21 14:41:19 +0530

    Revert "fixing config problems"
    This reverts commit d96e8eb5f7411139f6b943aa9a786bcdb8d4d618.

--- tests/config.py
+++ tests/config.py
@@ -30,7 +30,7 @@ UART_CONFIG = {
     "INVALID_TILL_VALUE": "Invalid till value",
     "VALID_WRITE_VALUE": "hello",
     "INVALID_WRITE_VALUE": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "VALID_DATA_RESPONSE": "Serial write Successful",
+    "VALID_DATA_RESPONSE": "Serial write Successfull",
     "INVALID_DATA_RESPONSE": "Command timed out"
 }
 
